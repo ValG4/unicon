7ce50923321c63e01364ae94841831e5c26a1f6e
Valentin GOYET <valentin.goyet3@gmail.com>
2024-12-07 06:54:32 +0100

    Test Framework and logs file

---
+++ confdefs.h
@@ -0,0 +1,8 @@
+/* confdefs.h */
+#define PACKAGE_NAME "Unicon"
+#define PACKAGE_TARNAME "unicon"
+#define PACKAGE_VERSION "13.3"
+#define PACKAGE_STRING "Unicon 13.3"
+#define PACKAGE_BUGREPORT "jeffery@unicon.org"
+#define PACKAGE_URL "http://www.unicon.org"
+#define Windows 1
